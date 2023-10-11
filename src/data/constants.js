f91c988de69b639c736bb244feaec55b61bf337c
sushavanpalit <sp2430@cse.jgec.ac.in>
2023-10-12 01:14:00 +0530

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,240 @@
+export const Bio = {
+  name: "SUSHAVAN PALIT",
+  roles: [
+    "Front-End Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/sushavanpalit",
+  resume:
+    "https://drive.google.com/file/d/1J6EuXE5jM0CyUBQyE_mXxNhewYpuSIOP/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/sushavan-palit-952408203/",
+  facebook: "https://www.facebook.com/sushovan.palit.1/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+
+    ],
+  },
+  {
+    title: "Language",
+    skills: [
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/800px-ISO_C%2B%2B_Logo.svg.png",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://db.cs.uni-tuebingen.de/teaching/ws2223/sql-is-a-programming-language/logo.svg",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+
+
+    ],
+  },
+
+];
+
+export const experiences = [
+
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D560BAQFV0JYJ33yONg/company-logo_200_200/0/1691701551582?e=2147483647&v=beta&t=QkrAq-RgRFTB6KVkKq2C_E7toRJXL27_M-cVoR_smSk",
+    role: "Frontend Engineer Intern",
+    company: "Bharat Intern",
+    date: "september 2023 - October 2023",
+    desc: "Working on the frontend of the web application using HTML, CSS, ReactJS and Material UI.",
+    skills: [
+      "ReactJS",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    link: "https://drive.google.com/file/d/1tDDehAyVhDkt3b2xhbsK2_Te1IcnQdFc/view?usp=sharing",
+    doc: "https://freepngimg.com/download/icon/1000463-index-finger-emoji-[free-download-ios-emojis]-icon-download-free.png",
+  },
+
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/9/90/JGEC_logo.png",
+    school: "Jalpaiguri Government Engineering College , West Bengal",
+    date: "2020 -  2024",
+    grade: "8.51 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Jalpaiguri Government Engineering College, West Bengal. I have completed 6 semesters and have a CGPA of 8.51. I have taken courses in Data Structures And Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - B.Tech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://directory.edugorilla.com/wp-content/uploads/sites/6/2018/06/09fec3e2d836dcd3bf21d9d393adb710-300x169.jpeg",
+    school: "ICHLABAD HIGH SCHOOL,PURBA BARDHAMAN.",
+    date: "2018 - 2020",
+    grade: "85.6%",
+    desc: "I completed my class 12  at CICHLABAD HIGH SCHOOL,PURBA BARDHAMAN.",
+    degree: "WBCHSE(XII)",
+  },
+  {
+    id: 2,
+    img: "https://school.banglarshiksha.gov.in/sms/templates/uploads/ws/26662/8589.jpg",
+    school: "DUBRAJHAT BERUGRAM High School, PURBA BARDHAMAN.",
+    date: " -2018",
+    grade: "86.28%",
+    desc: "I completed my class 10  at DUBRAJHAT BERUGRAM High School, PURBA BARDHAMAN.",
+    degree: "WBBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "WEATHER APP",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "A WEATHER APP made with React JS, Javascript, and Material UI. It is a platform resembling Udemy, allowing users to create, sell, and take online courses, fostering a virtual learning marketplace for a wide range of subjects and skills.",
+    image:
+      "https://raw.githubusercontent.com/alexkowsik/react-weather-app/master/src/images/screenshot.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "Java Script",
+      "React Js",
+      "Redux",
+    ],
+    category: "web app",
+  },
+  {
+    id: 0,
+    title: "Temperature Converter",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Temperature conversion is a process of converting a temperature value from one scale (e.g., Celsius) to another (e.g., Fahrenheit or Kelvin) for different applications and understanding temperature in different contexts.",
+    image:
+      "https://i.ibb.co/LYhyjX7/Screenshot-3.png",
+    tags: ["HTML", "CSS", "Java Script",],
+    category: "web app",
+
+  },
+  {
+    id: 1,
+    title: "Analog Clock",
+    date: "Dec 2022 - Dec 2022",
+    description:
+      "An analog clock is a timekeeping device with moving hands (hour, minute, and sometimes second) on a circular face, displaying the current time through their positions and angles.",
+    image:
+      "https://www.american-time.com/wp-content/uploads/2021/02/01-15in-Black-Steel-Round-Surface-Mount.jpg",
+    tags: [
+      "HTML",
+      "CSS",
+      "Java Script",
+    ],
+    category: "web app",
+  },
+
+  {
+    id: 3,
+    title: "Stopwatch",
+    date: "Jan 2023 - Jan 2023",
+    description:
+      "App Is Currently In Playstore 100+ Downloads. This Project proposes an “Expense Tracking App”. Keep track of your personal expenses and compare them to your monthly income with the budget planner. It has Google Drive Cloud API for Backup of User Room Database. Made with Kotlin in MVVM Architecture & Live Data.",
+    image:
+      "https://i.ibb.co/86dBN7B/Screenshot-5.png",
+    tags: ["HTML", "CSS", "Java Script",],
+    category: "Web-app",
+  },
+];
+
+// export const TimeLineData = [
+//   { year: 2017, text: "Started my journey" },
+//   { year: 2018, text: "Worked as a freelance developer" },
+//   { year: 2019, text: "Founded JavaScript Mastery" },
+//   { year: 2020, text: "Shared my projects with the world" },
+//   { year: 2021, text: "Started my own platform" },
+// ];
